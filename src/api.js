e2a0e30e2ea0940a1d7e1f7829c140460fb7bbb8
adithyan s kumar <adithyans1438@gmail.com>
2024-12-08 12:47:33 +0530

    server domain change

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
-export const STREAM_URL = "https://cineflixserver.onrender.com/api/movies/stream-video"
+export const STREAM_URL = "https://cineflix.unitedsrlogistics.com/api/movies/stream-video"
 
-export const REACT_APP_API_URL = "https://cineflixserver.onrender.com/api/"
+export const REACT_APP_API_URL="https://cineflix.unitedsrlogistics.com/api/"
+
