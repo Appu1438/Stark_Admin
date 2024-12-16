0618058ce7147e1b341b5d83be8e22421c95dfb9
adithyan s kumar <adithyans1438@gmail.com>
2024-12-16 15:20:25 +0530

    versel url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
-export const STREAM_URL = "https://cineflix.unitedsrlogistics.com/api/movies/stream-video"
+export const STREAM_URL = "https://cineflix-server.vercel.app/api/movies/stream-video"
+// export const STREAM_URL = "http://localhost:5000/api/movies/stream-video"
 
-export const REACT_APP_API_URL="https://cineflix.unitedsrlogistics.com/api/"
+export const REACT_APP_API_URL="https://cineflix-server.vercel.app/api/"
+// export const REACT_APP_API_URL="http://localhost:5000/api/"
 
