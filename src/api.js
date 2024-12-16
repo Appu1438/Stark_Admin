33ac3f4b1cbad57aa783e2cd54d2857a9a75e62f
adithyan s kumar <adithyans1438@gmail.com>
2024-12-16 15:34:32 +0530

    render url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-export const STREAM_URL = "https://cineflix-server.vercel.app/api/movies/stream-video"
+export const STREAM_URL = "https://cineflixserver.onrender.com/api/movies/stream-video"
 // export const STREAM_URL = "http://localhost:5000/api/movies/stream-video"
 
-export const REACT_APP_API_URL="https://cineflix-server.vercel.app/api/"
+export const REACT_APP_API_URL="https://cineflixserver.onrender.com/api/"
 // export const REACT_APP_API_URL="http://localhost:5000/api/"
 
